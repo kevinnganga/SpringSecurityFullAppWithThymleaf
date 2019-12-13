b9e1acb9a632628769b2edaed9f9d3f1fe581d9a
kevin nganga <kevin.nganga07@gmail.com>
2019-12-13 10:44:13 +0300

    Configure Role Based Authorisation

--- FinalBestSpringSecurityWithThymleaf/src/main/java/Java_BrainsSecurity/Java_BrainsSecurity/SecurityConfiguration.java
+++ FinalBestSpringSecurityWithThymleaf/src/main/java/Java_BrainsSecurity/Java_BrainsSecurity/SecurityConfiguration.java
@@ -24,14 +24,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
         	 .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
         	 .and()
-        	 .withUser("dan").password(passwordEncoder().encode("dan123")).roles("USER");
+        	 .withUser("dan").password(passwordEncoder().encode("dan123")).roles("USER")
+             .and()
+   	         .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         
     	http.authorizeRequests()
-    	.anyRequest().authenticated()
+    	.antMatchers("/index.html").permitAll()//START WITH THE LEAST PRIVILLEGED COZ EVERY ROLE CAN ACCESS EVERYTHING ABOVE IT eg ADMIN CAN ACCESS /profile/index and /index.html
+    	.antMatchers("/profile/index").authenticated() //No html extension coz its the only file in the folder admin
+    	.antMatchers("/admin/index").hasRole("ADMIN")
+    	.antMatchers("/management/index").hasAnyRole("ADMIN","MANAGER")
     	.and()
     	.httpBasic();
                
